406c271370fd0e7be67024bc1f1287d84467c9d9
Changlin Li <mail@changlinli.com>
2025-05-14 14:28:01 -0400

    Finish up the exercises

--- vanilla-neural-net-mech-interp/exercises/exercises.py
+++ vanilla-neural-net-mech-interp/exercises/exercises.py
@@ -10,6 +10,39 @@ import einops
 
 # %%
 
+# Some nice preliminary functions for testing.
+
+def assert_with_expect(expected, actual):
+    assert expected == actual, f"Expected: {expected} Actual: {actual}"
+
+
+def assert_list_of_floats_within_epsilon(
+    expected: list[float], 
+    actual: list[float],
+    eps=0.0001,
+):
+    if len(expected) != len(actual):
+        raise AssertionError(f"Expected: {expected} Actual: {actual}")
+    is_within_eps = True
+    for e, a in zip(expected, actual):
+        is_within_eps = is_within_eps and abs(e - a) < eps
+    if not is_within_eps:
+        raise AssertionError(f"Expected: {expected} Actual: {actual}")
+
+
+def assert_tensors_within_epsilon(
+    expected: torch.Tensor,
+    actual: torch.Tensor,
+    eps=0.001,
+):
+    if expected.shape != actual.shape:
+        raise AssertionError(f"Shapes of tensors do not match! Expected: {expected.shape} Acutal: {actual.shape}")
+    differences_within_epsilon = abs(expected - actual) < eps
+    if not differences_within_epsilon.all():
+        raise AssertionError(f"Values of tensors do not match! Expected: {expected} Actual: {actual}")
+
+# %%
+
 # None of this is code that you will need to write, but you should read this
 # over to understand the structure of what kind of nets we'll be training.
 #
@@ -49,7 +82,6 @@ class SimpleNN(nn.Module):
         x2 = self.fc2(x1)
         return self.softmax(x2)
 
-
 # %%
 
 # This is useful for creating reproducible tests for whether you wrote the code
@@ -59,14 +91,15 @@ example_simple_nn = SimpleNN(EXAMPLE_SIMPLE_NN_HIDDEN_DIM)
 
 # We'll hard-code all of these values so that they are reproducible for tests
 with torch.no_grad():
-  fc1_weights = einops.rearrange(torch.arange(784 * EXAMPLE_SIMPLE_NN_HIDDEN_DIM), "(x y) -> x y", x=EXAMPLE_SIMPLE_NN_HIDDEN_DIM, y=784)
+  fc1_weight = einops.rearrange(torch.arange(784 * EXAMPLE_SIMPLE_NN_HIDDEN_DIM), "(x y) -> x y", x=EXAMPLE_SIMPLE_NN_HIDDEN_DIM, y=784)
   fc1_bias = torch.arange(EXAMPLE_SIMPLE_NN_HIDDEN_DIM)
-  fc2_weights = einops.rearrange(torch.arange(10 * EXAMPLE_SIMPLE_NN_HIDDEN_DIM), "(x y) -> x y", x=10, y=EXAMPLE_SIMPLE_NN_HIDDEN_DIM)
+  fc2_weight = einops.rearrange(torch.arange(10 * EXAMPLE_SIMPLE_NN_HIDDEN_DIM), "(x y) -> x y", x=10, y=EXAMPLE_SIMPLE_NN_HIDDEN_DIM)
   fc2_bias = torch.arange(10)
-  example_simple_nn.fc1.weights = torch.nn.Parameter(fc1_weights.to(torch.float))
+  example_simple_nn.fc1.weight = torch.nn.Parameter(fc1_weight.to(torch.float))
   example_simple_nn.fc1.bias = torch.nn.Parameter(fc1_bias.to(torch.float))
-  example_simple_nn.fc2.weights = torch.nn.Parameter(fc2_weights.to(torch.float))
+  example_simple_nn.fc2.weight = torch.nn.Parameter(fc2_weight.to(torch.float))
   example_simple_nn.fc2.bias = torch.nn.Parameter(fc2_bias.to(torch.float))
+
 
 # %%
 
@@ -79,7 +112,7 @@ test_dataset = datasets.MNIST('./data', train=False, transform=transform)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=10000)
 
-hidden_dims = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072]
+hidden_dims = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144]
 models = [SimpleNN(hidden_dim) for hidden_dim in hidden_dims]
 
 # %%
@@ -223,7 +256,11 @@ for model in models:
 
 # %%
 
+# Fill this out! This should calculate the accuracy of the model for each digit.
+
 def accuracy_by_digit(model, loader):
+  # TODO: Implement this!
+  #raise NotImplementedError()
   correct = [0] * 10
   total = [0] * 10
   with torch.no_grad():
@@ -249,13 +286,38 @@ accuracy_by_digit(models[0], test_loader)
 
 # %%
 
+# Okay now we want to reframe our model using the key-value visualization
+# So it's time to implement the functions that will pull out the ith key and ith
+# value in our neural net.
+
+def pull_out_ith_key(model, i):
+  # TODO: Implement this (it should be just one line)
+  # raise NotImplementedError()
+  return model.fc1.weight[i]
+
+def pull_out_ith_value(model, i):
+  # TODO: Implement this (again should just be one line)
+  # raise NotImplementedError()
+  return model.fc2.weight[:, i]
+
+# Test code to make sure your code works!
+assert_tensors_within_epsilon(expected=torch.arange(1568, 2352), actual=pull_out_ith_key(example_simple_nn, 2))
+assert_tensors_within_epsilon(
+   expected=torch.tensor([ 2.,  8., 14., 20., 26., 32., 38., 44., 50., 56.]),
+   actual=pull_out_ith_value(example_simple_nn, 2)
+)
+
+# %%
+
+# This is all a lot of visualization code which you can either read or just run.
+
 def visualize_image(image):
   plt.imshow(image.detach().numpy(), cmap='viridis')
   plt.axis('off')
   plt.show()
 
 def visualize_ith_key(model, i):
-  key = model.fc1.weight[i].reshape(28, 28)
+  key = pull_out_ith_key(model, i).reshape(28, 28)
   key_bias = model.fc1.bias[i]
   plt.imshow(key.detach().numpy(), cmap='viridis')
   plt.axis('off')
@@ -263,7 +325,7 @@ def visualize_ith_key(model, i):
   plt.show()
 
 def visualize_ith_value(model, i):
-  value = model.fc2.weight[:, i].unsqueeze(0)
+  value = pull_out_ith_value(model, i).unsqueeze(0)
   plt.imshow(value.detach().numpy(), cmap='viridis')
   for x in range(value.shape[1]):
     plt.text(x, 0, f'{value[0, x].item():.3f}', ha='center', va='center', color='red', fontsize=6)
@@ -300,21 +362,44 @@ def visualize_ith_key_value_on_image(model, i, image):
 
 # %%
 
+# Now that we have a way of pulling out keys and values, we can put that all
+# together to visualize a particular key-value pair!
+#
+# You might notice that this particular key (if you're using the pre-trained
+# model weights) looks visually kind of like a nine, and lo and behold, when you
+# go to the value vector that is getting written out, the highest activation is
+# a 9!
+
 visualize_ith_key_value(models[14].cpu(), 246)
 
+# Go ahead and play around with other key value pairs and see if you can make
+# sense of them.
+
 # %%
+
+# It's often useful to find which value vectors we have that tend to write
+# strongly for certain kinds of digits.
+#
+# Here is one very rough stab at the problem that just looks for any value
+# vector that has a value over a certain threshold for that digit. We'll quickly
+# show a slightly less rough stab in just a moment.
 
 def find_values_for_digit_over_threshold(model, digit, threshold=0.3):
   return torch.tensor([idx for idx in range(model.fc2.weight.shape[1]) if model.fc2.weight[digit, idx] > threshold])
 
 # %%
 
+# This finds those values which have an entry of over 0.4 in the 0 digit place.
+
 find_values_for_digit_over_threshold(models[14], 0, threshold=0.4)
+
+# Feel free to feed this into visualize_ith_key_value to see what that key_value pair looks like!
 
 # %%
 
-# Let's see some of how this key-value reframing of
-# If we see a value vector that 
+# Let's see a little bit more of how this key-value reframing of a vanilla
+# neural net can help us understand things better.
+#
 # For example, we might hypothesize that the key which corresponds to a value vector
 # that has a large positive value at 0 and small magnitude values for all other digits
 # should look like a circle.
@@ -324,7 +409,11 @@ find_values_for_digit_over_threshold(models[14], 0, threshold=0.4)
 # with no key actually being a full circle.
 #
 # But we can go ahead and test that right now. First we'll need to build a
-# function that 
+# function that can find those key-value pairs which have values concentrated
+# mostly on one digit and not as much on the others.
+#
+# This can be a bit finicky and hard to specify, so we've provided a
+# rough-and-tumble version for you to use right here.
 
 def find_values_for_sole_digit(model, digit, digit_threshold=0.16, other_digits_threshold=0.07):
   result = []
@@ -340,11 +429,16 @@ def find_values_for_sole_digit(model, digit, digit_threshold=0.16, other_digits_
 
 # %%
 
+# So this for example finds those key-value pairs which tend to write very
+# strongly to the digit 2 happening, but very little for anything else.
 find_values_for_sole_digit(models[14], 2)
 
 # %%
 
-# Let's do some quick sanity checks
+# Let's now find those key-value pairs which tend to write strongly to the digit
+# 0, but very little for everything else, and just analyze the first three of
+# those key-value pairs. This will let us validate our hypothesis of whether we
+# have keys that are looking for circles, or just fragmentary arcs of circles.
 
 digit_to_analyze = 0
 
@@ -354,14 +448,21 @@ for idx in indices_that_fire_mainly_on_select_digit[:3]:
 
 # %%
 
-models[14].fc1.weight[0].shape
-models[14].fc2.weight[:,0].shape
+# TODO: Look at the results and what they tell you. Talk with your partner or
+# group about what you're seeing. Once you've done that, delete this
+# NotImplementedError and move on.
+# raise NotImplementedError()
 
 # %%
 
-models[14].fc2.weight.shape
-
-# %%
+# This function will give us the internal outputs of all the keys and values for
+# a given image. In other words this will return the dot product of each key
+# with the image (combined with the bias per key) and will also return the
+# scaled value vector.
+#
+# If this is confusing to you, it may be helpful to go back to the slides and
+# look a little bit more at the break-down of how exactly we calculate a neural
+# net's output using the key-value paradigm.
 
 def compute_kv_outputs_for_image(model, input_image):
   flattened_img = model.flatten(input_image)
@@ -372,10 +473,10 @@ def compute_kv_outputs_for_image(model, input_image):
   # standard element-wise tensor multiplication
   #
   # But this is just the same as broadcasting, so we just use that instead
-  print(f"{output_after_relu.shape=}")
-  print(f"{model.fc2.weight.shape=}")
   output_after_values = model.fc2.weight * output_after_relu
   return output_after_keys, output_after_values
+
+# %%
 
 def top_indices_by_tail_sum(tensor: torch.Tensor, threshold: float) -> torch.Tensor:
     """
@@ -407,23 +508,27 @@ print(f"{indices=}")  # tensor([1, 3])
 
 
 def list_top_kv_pair_idxs(model, input_image, excess_abs_weight=500):
-  output_after_keys, output_after_values = compute_kv_outputs_for_image(model, input_image)
+  _, output_after_values = compute_kv_outputs_for_image(model, input_image)
   abs_values = einops.einsum(torch.abs(output_after_values), "digits num_of_values -> num_of_values")
   indices = top_indices_by_tail_sum(abs_values, excess_abs_weight)
   return indices
 
 # %%
 
+# Let's prove to ourselves that the key-value paradigm of calculating things actually is equal to
 def sanity_check_kv_outputs(model, input_image):
   _, output_after_values = compute_kv_outputs_for_image(model, input_image)
   output_plus_bias = einops.einsum(output_after_values, "digits num_of_values -> digits") + model.fc2.bias
   print(f"{output_plus_bias.softmax(dim=-1)=}")
   print(f"{model(input_image)=}")
 
-sanity_check_kv_outputs(models[14], train_dataset[0][0].to("cpu"))
+# You should see that the two print statements print the same values
+sanity_check_kv_outputs(models[14], train_dataset[0][0].cpu())
 
 # %%
 
+# This will list the key-value pairs that write the value vectors with the largest magnitude.
+# This is a proxy for 
 list_top_kv_pair_idxs(models[14], train_dataset[0][0].cpu(), 7000)
 
 # %%
@@ -444,7 +549,7 @@ def sort_by_value_variance(model, input_image):
   print(f"{var_indices.shape=}")
   return var_indices
 
-sort_by_value_variance(models[14], train_dataset[0][0].cpu())[:, :5]
+top_5_kv_pairs_by_value_variance = sort_by_value_variance(models[14], train_dataset[0][0].cpu())[:, :5]
 
 # %%
 
@@ -499,6 +604,8 @@ models[0].cpu()(train_dataset[3][0].cpu()).squeeze()
 visualize_ith_key_value_on_image(models[0].cpu(), 7, train_dataset[3][0].cpu().squeeze())
 
 # %%
+
+# This finds the image that activates mostly strongly for a given key.
 
 def sort_highest_activating_image_for_key(model, key_value_idx, input_images):
   key = model.fc1.weight[key_value_idx, :]
@@ -567,12 +674,15 @@ def knock_out_ith_key(model: SimpleNN, key_value_idx: torch.Tensor) -> SimpleNN:
 
 # %%
 
+# Find all those key-value pairs which activate a lot for zero
 all_values_that_activate_significantly_for_zero = find_values_for_digit_over_threshold(models[14], 0, threshold=0.1)
 
 # %%
 
+# Let's see if we can just selectively knock those out!
 model_with_0_knocked_out = knock_out_ith_key(models[14], all_values_that_activate_significantly_for_zero)
 
 # %%
 
+# And now we see that the model is basically entirely incapable of recognizing 0, but the rest of its capabilities are left intact!
 accuracy_by_digit(model_with_0_knocked_out.to(DEVICE), test_loader)
