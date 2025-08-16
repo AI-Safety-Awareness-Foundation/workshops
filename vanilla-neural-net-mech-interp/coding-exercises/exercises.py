419139fd63555ca940ff95f867d589be80ef9bc4
Changlin Li <mail@changlinli.com>
2025-08-16 15:29:06 -0500

    Make sure that exercises work

--- vanilla-neural-net-mech-interp/coding-exercises/exercises.py
+++ vanilla-neural-net-mech-interp/coding-exercises/exercises.py
@@ -112,7 +112,7 @@ test_dataset = datasets.MNIST('./data', train=False, transform=transform)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=10000)
 
-hidden_dims = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144]
+hidden_dims = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072]
 models = [SimpleNN(hidden_dim) for hidden_dim in hidden_dims]
 
 # %%
